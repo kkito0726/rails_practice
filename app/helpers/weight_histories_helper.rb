c7d8e5a17e9b9135355600019d7f2d5ef6f54071
kkito0726 <k.kito0726@gmail.com>
2023-01-15 22:08:30 +0900

    体重によってテキストを変える

--- app/helpers/weight_histories_helper.rb
+++ app/helpers/weight_histories_helper.rb
@@ -1,2 +1,11 @@
 module WeightHistoriesHelper
+  def weight_comment(weight)
+    if weight < 20
+      return "#{weight} kgは痩せ型です"
+    elsif weight < 30
+      return "#{weight} kgは標準型です"
+    else
+      return "#{weight} kgは太り気味です"
+    end
+  end
 end
